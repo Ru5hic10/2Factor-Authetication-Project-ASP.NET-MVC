6929d9201e2da850f55952a478ee5d904a96422a
Ru5hi <Rushalchavan1059@gmail.com>
2023-05-17 23:53:18 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2FA_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
